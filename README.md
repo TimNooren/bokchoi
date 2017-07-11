a632100f9ddf106c127cabcf3aa4d1313a45cf0e
tim.nooren <tim.nooren@debijenkorf.nl>
2017-07-11 14:19:00 +0200

    Added README and LICENSE files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bokchoi
+
+Bokchoi simplifies running Python batch jobs on AWS spot instances. Bokchoi handles requesting spot instances, deploying your code and ensures the spot requests are cancelled when all jobs are finished.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Installing
+
+Installing bokchoi:
+
+```
+pip install bokchoi
+```
--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+
+import os
 from setuptools import setup
+
+
+try:
+    from pypandoc import convert
+    README = convert('README.md', 'rst')
+except ImportError:
+    README = open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r', encoding="utf-8").read()
 
 setup(
     name="bokchoi",
@@ -8,8 +17,23 @@ setup(
         'Click',
         'boto3'
     ],
-    entry_points='''
-        [console_scripts]
-        bokchoi=bokchoi.cli:cli
-    ''',
+    url='https://github.com/TimNooren/bokchoi',
+    author='Tim Nooren',
+    author_email='timnooren@gmail.com',
+    long_description=README,
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'bokchoi=bokchoi.cli:cli'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
