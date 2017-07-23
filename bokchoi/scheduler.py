01acdb624b8eec0f8b1d9b38adfa07e776b336c3
tim.nooren <tim.nooren@debijenkorf.nl>
2017-07-23 12:24:49 +0200

    Updated scheduler to get settings correctly

--- bokchoi/scheduler.py
+++ bokchoi/scheduler.py
@@ -10,16 +10,16 @@ def run(event, context):
 
     import main
 
-    settings = main.load_settings()
-    job = settings[PROJECT]
+    settings = main.load_settings(PROJECT)
+
     job_id = main.create_job_id(PROJECT)
 
     bucket_name = job_id
     zip_file_name = 'bokchoi-{}.zip'.format(PROJECT)
 
-    ec2_settings = job['EC2']
+    ec2_settings = settings['EC2']
 
-    app, entry = job['EntryPoint'].split('.')
+    app, entry = settings['EntryPoint'].split('.')
     user_data = main.USER_DATA.format(bucket=bucket_name, package=zip_file_name, app=app, entry=entry)
     ec2_settings['LaunchSpecification']['UserData'] = base64.b64encode(user_data.encode('ascii')).decode('ascii')
 
