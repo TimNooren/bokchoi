0dca400ca4c3eafe1f74fb352953e6293572e773
tim.nooren <tim.nooren@debijenkorf.nl>
2018-09-17 14:59:01 +0200

    0.4.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name="bokchoi",
-    version="0.3.4",
+    version="0.4.0",
     packages=['bokchoi', 'bokchoi.aws', 'bokchoi.gcp'],
     install_requires=[
         'Click',
