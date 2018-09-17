3df559919ff5cb35350473d4766d6537224c4156
tim.nooren <tim.nooren@debijenkorf.nl>
2018-09-17 14:58:43 +0200

    Changed logging source names to bokchoi for init and shutdown and app

--- bokchoi/aws/ec2-startup-script.sh
+++ bokchoi/aws/ec2-startup-script.sh
@@ -17,13 +17,13 @@ python3 -c "import zipfile; zf = zipfile.ZipFile('/tmp/{package}'); zf.extractal
 sudo chmod u+x /tmp/cloudwatch_logger.py
 sed -i $'s/\\r$//' /tmp/cloudwatch_logger.py
 
-echo "Downloaded and unpacked project zip" | /tmp/cloudwatch_logger.py init
+echo "Downloaded and unpacked project zip" | /tmp/cloudwatch_logger.py bokchoi
 
 # Install pip3 and install requirements.txt from project zip if included
 curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3
 [ -f /tmp/requirements.txt ] && pip3 install -r /tmp/requirements.txt && pip3 install boto3
 
-echo "Installed requirements" | /tmp/cloudwatch_logger.py init
+echo "Installed requirements" | /tmp/cloudwatch_logger.py bokchoi
 
 if [ "{notebook}" = "True" ]
 then
@@ -40,17 +40,17 @@ else
     # Run app
     cd /tmp
 
-    echo "Running app" | /tmp/cloudwatch_logger.py init
+    echo "Running app" | /tmp/cloudwatch_logger.py bokchoi
 
     python3 -u {entrypoint} | ./cloudwatch_logger.py app
     aws s3 cp /var/log/cloud-init-output.log s3://{bucket}/cloud-init-output.log
 
-    echo "Finished running app" | /tmp/cloudwatch_logger.py end
+    echo "Finished running app" | /tmp/cloudwatch_logger.py bokchoi
 
     if [ "{shutdown}" = "True" ]
     then
-        echo "Shutting down..." | /tmp/cloudwatch_logger.py end
-        echo "log-termination" | /tmp/cloudwatch_logger.py end
+        echo "Shutting down..." | /tmp/cloudwatch_logger.py bokchoi
+        echo "log-termination" | /tmp/cloudwatch_logger.py bokchoi
         shutdown -h now
     fi
 
